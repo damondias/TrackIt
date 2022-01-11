29694514c1ffda360a73083523f922c90f073388
Damon <damondias@gmail.com>
2022-01-11 02:46:53 -0400

    implementando pagina dde habitos

--- src/pages/SignUpPage/styles.js
+++ src/pages/SignUpPage/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 
 const Container = styled.div`
-     font-family: 'Roboto', sans-serif;
+    font-family: 'Roboto', sans-serif;
     font-size: 14px;
     
     min-height: 100vh;
